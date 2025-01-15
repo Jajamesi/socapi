93ad3e478271bd8b4cb930f906c4196a02cc1a3b
jajames <james14881@gmail.com>
2025-01-15 15:23:14 +0300

    _meta_parser draft

---
+++ src/socapi/_meta_parser.py
@@ -0,0 +1,13 @@
+
+import asyncio
+
+from . import constants as const
+from . import utils
+
+# https://admin.world-survey.com/api/block/getbypoll - block metadata
+# https://admin.world-survey.com/api/question/getbypoll - all questions metadata
+# https://admin.world-survey.com/poll/5874/blocks
+
+# class MetaParser():
+
+    # async def _parse
