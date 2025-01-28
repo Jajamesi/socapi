f5e58980e6329bf1b1878cbdc826c6541f342113
jajames <james14881@gmail.com>
2025-01-28 10:19:02 +0300

    search by number

--- src/socapi/_searcher.py
+++ src/socapi/_searcher.py
@@ -4,8 +4,6 @@ import asyncio
 from win32com.client import constants
 
 from . import constants as const
-from . import utils
-from . import constants
 
 
 class Searcher:
@@ -56,4 +54,54 @@ class Searcher:
 
     def search_by_name(self, name: str, *args, **kwargs):
         return asyncio.run(self._search_by_name(name, *args, **kwargs))
+
+
+    async def _search_by_number(
+            self,
+            num: int,
+            is_in_track=False,
+            chunk_size=50
+    ):
+        await self._login()
+
+        return_dict = dict()
+        counter = 0
+
+        while True:
+            search_payload = {
+                "is_in_track": is_in_track,
+                "num": num,
+                "limit": chunk_size + 1,
+                "offset": chunk_size * counter
+            }
+
+            result = await self._request(
+                endpoint=const.SEARCH_LIST_URL,
+                request_name="Search by name",
+                headers=self.headers,
+                payload=search_payload,
+            )
+
+            try:
+                result_json = await result.json()
+            except:
+                raise ValueError("Failed to json search by number")
+
+            if result_json.get("error") != "":
+                raise ValueError(f"Error in search by number: {result_json.get("error")}")
+
+            chunk = {poll["id"]: {par: poll[par] for par in constants.SEARCH_RETURNS} for poll in result_json["result"]}
+            return_dict.update(chunk)
+
+            if len(chunk) < chunk_size:
+                break
+
+            counter += 1
+
+        return return_dict
+
+
+    def search_by_number(self, name: str, *args, **kwargs):
+        return asyncio.run(self._search_by_name(name, *args, **kwargs))
+
 
